d2b236f2d8bedd54d4c56554e1aaaee5ebd90239
Artik <Artik25942@gmail.com>
2022-01-20 09:51:39 +0200

    delete fetch type

--- src/main/java/cinema/model/Ticket.java
+++ src/main/java/cinema/model/Ticket.java
@@ -1,6 +1,12 @@
 package cinema.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tickets")
@@ -8,7 +14,7 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "movie_session_id")
     private MovieSession movieSession;
     @ManyToOne
